ee522c1193668c36763e692032116690aeb5f9ff
Fabian Paischer <paischer101@gmail.com>
2019-03-24 14:08:11 +0100

    class distribution plot refinement

--- Audio_Tagging/plot_statistics.py
+++ Audio_Tagging/plot_statistics.py
@@ -8,28 +8,32 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-year', required=True)
 args = parser.parse_args()
 
-def plot_class_distribution(type):
-    if type == 'verified':
-        datapoints = get_verified_files_dict(args.year)
-    else:
-        datapoints = get_unverified_files_dict(args.year)
+def plot_class_distribution():
+    verified_files = get_verified_files_dict(args.year)
+    unique_verified = np.unique(list(verified_files.values()))
+    print(len(unique_verified))
+    counter = Counter(verified_files.values())
+    counts_verified = [counter[label] for label in unique_verified]
 
-    unique = np.unique(list(datapoints.values()))
-    print(len(unique))
-    counter = Counter(datapoints.values())
-    counts = [counter[label] for label in unique]
+    unverified_files = get_unverified_files_dict(args.year)
+    unique_unverified = np.unique(list(unverified_files.values()))
+    print(len(unique_unverified))
+    counter = Counter(unverified_files.values())
+    counts_unverified = [counter[label] for label in unique_unverified]
+
     plt.figure(figsize=(15,15))
-    plt.bar(unique, counts)
-    plt.xticks(range(len(unique)), unique, rotation='vertical')
-    plt.title('Class distribution of {} labels'.format(type))
+    plt.bar(unique_verified, counts_verified, label='verified files')
+    plt.bar(unique_unverified, counts_unverified, label='unverified files')
+    plt.xticks(range(len(unique_verified)), unique_verified, rotation='vertical')
+    plt.title('Class distribution')
     plt.xlabel('Classes')
     plt.ylabel('Frequency')
-    plt.gcf().savefig('plots/class_distribution_{}.png'.format(type))
+    plt.legend(loc='upper right')
+    plt.gcf().savefig('plots/class_distribution.png')
     plt.close()
 
 def main():
-    plot_class_distribution('verified')
-    plot_class_distribution('unverified')
+    plot_class_distribution()
 
 if __name__ == '__main__':
     main()
