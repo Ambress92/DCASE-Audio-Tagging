9b2a6177cda30ffe3f4421a34ba81d640a69bdd5
Fabian Paischer <paischer101@gmail.com>
2019-03-24 17:18:03 +0100

    feature extractor added

--- Audio_Tagging/dataloader.py
+++ Audio_Tagging/dataloader.py
@@ -1,6 +1,7 @@
 from scipy.io import wavfile
 import tqdm
 import numpy as np
+import yaml
 
 def get_verified_files_dict(year):
     with open('../datasets/{}/train.csv'.format(year), 'r') as in_file:
@@ -20,32 +21,42 @@ def get_unverified_files_dict(year):
 
     return unverified_files_dict
 
-def load_verified_files(year):
+def load_verified_files(year, load_features=False):
     verified_files_dict = get_verified_files_dict(year)
 
     # load verified audio clips
     verified_files = []
     for file, label in tqdm.tqdm(zip(verified_files_dict.keys(), verified_files_dict.values()), 'Loading verified clips'):
-        _, data = wavfile.read('../datasets/{}/audio_train/{}'.format(year, file))
+        if not load_features:
+            _, data = wavfile.read('../datasets/{}/audio_train/{}'.format(year, file))
+        else:
+            with open('../features/{}/mfcc/{}'.format(year, file), 'r') as in_file:
+                data = yaml.load(in_file)
+
         verified_files.append((data, label))
 
     return verified_files
 
-def load_unverified_files(year):
+def load_unverified_files(year, load_features=False):
     unverified_files_dict = get_unverified_files_dict(year)
 
     # load verified audio clips
-    verified_files = []
+    unverified_files = []
     for file, label in tqdm.tqdm(zip(unverified_files_dict.keys(), unverified_files_dict.values()),
                                  'Loading verified clips'):
-        _, data = wavfile.read('../datasets/{}/audio_train/{}'.format(year, file))
-        verified_files.append((data, label))
+        if not load_features:
+            _, data = wavfile.read('../datasets/{}/audio_train/{}'.format(year, file))
+        else:
+            with open('../features/{}/mfcc/{}'.format(year, file), 'r') as in_file:
+                data = yaml.load(in_file)
 
-    return unverified_files_dict
+        unverified_files.append((data, label))
+
+    return unverified_files
 
 def get_label_mapping():
     verified_files_dict = get_verified_files_dict()
-    labels = np.unique(verified_files_dict.values())
+    labels = np.unique(list(verified_files_dict.values()))
     label_mapping = {label: index for index, label in enumerate(labels)}
     inv_label_mapping = {v: k for k, v in zip(label_mapping.keys(), label_mapping.values())}
     return label_mapping, inv_label_mapping
--- Audio_Tagging/feature_extractor.py
+++ Audio_Tagging/feature_extractor.py
@@ -1,9 +1,52 @@
 import librosa
-import numpy as np
+import tqdm
+import argparse
+import os
+from scipy.io import wavfile
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-year', required=True)
+args = parser.parse_args()
+
+def dump_stft_specs():
+    files = os.listdir('../datasets/{}/audio_train'.format(args.year))
+
+    for file in tqdm.tqdm(files, 'Extracting mfccs'):
+        sr, data = wavfile.read('../datasets/{}/audio_train/{}'.format(args.year, file))
+
+        spec = librosa.stft(data)
+
+        with open('../features/{}/mfcc/{}.mfcc'.format(args.year, file.split('.')[0]), 'w') as out_file:
+            yaml.dump(spec, out_file)
+
+def dump_mel_specs():
+    files = os.listdir('../datasets/{}/audio_train'.format(args.year))
+
+    for file in tqdm.tqdm(files, 'Extracting mfccs'):
+        sr, data = wavfile.read('../datasets/{}/audio_train/{}'.format(args.year, file))
+
+        spec = librosa.feature.melspectrogram(data, sr)
+
+        with open('../features/{}/mfcc/{}.mfcc'.format(args.year, file.split('.')[0]), 'w') as out_file:
+            yaml.dump(spec, out_file)
+
+def dump_mfcc_features():
+    files = os.listdir('../datasets/{}/audio_train'.format(args.year))
+
+    for file in tqdm.tqdm(files, 'Extracting mfccs'):
+        sr, data = wavfile.read('../datasets/{}/audio_train/{}'.format(args.year, file))
+
+        mfcc = librosa.feature.mfcc(data, sr, n_mfcc=40)
+        #deltas = librosa.feature.delta(data)
+        #delta_delta = librosa.feature.delta(data, order=2)
+
+        with open('../features/{}/mfcc/{}.mfcc'.format(args.year, file.split('.')[0]), 'w') as out_file:
+           yaml.dump(mfcc.T, out_file)
 
 
 def main():
-    pass
+    dump_mfcc_features()
 
 if __name__ == '__main__':
     main()
