13c3e03bd3811d27693e8afacd69c0a9f41e36d5
Fabian Paischer <paischer101@gmail.com>
2019-03-25 13:53:44 +0100

    bugfixing feature extractor

--- Audio_Tagging/feature_extractor.py
+++ Audio_Tagging/feature_extractor.py
@@ -4,31 +4,32 @@ import argparse
 import os
 from scipy.io import wavfile
 import yaml
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-year', required=True)
 args = parser.parse_args()
 
-def dump_stft_specs():
+def dump_cqt_specs():
     files = os.listdir('../datasets/{}/audio_train'.format(args.year))
 
-    for file in tqdm.tqdm(files, 'Extracting mfccs'):
+    for file in tqdm.tqdm(files, 'Extracting stft features'):
         sr, data = wavfile.read('../datasets/{}/audio_train/{}'.format(args.year, file))
 
-        spec = librosa.stft(data)
+        spec = librosa.cqt(data.astype(np.float), sr=sr)
 
-        with open('../features/{}/mfcc/{}.mfcc'.format(args.year, file.split('.')[0]), 'w') as out_file:
+        with open('../features/{}/cqt/{}.cqt'.format(args.year, file.split('.')[0]), 'w') as out_file:
             yaml.dump(spec, out_file)
 
 def dump_mel_specs():
     files = os.listdir('../datasets/{}/audio_train'.format(args.year))
 
-    for file in tqdm.tqdm(files, 'Extracting mfccs'):
+    for file in tqdm.tqdm(files, 'Extracting mel spectrograms'):
         sr, data = wavfile.read('../datasets/{}/audio_train/{}'.format(args.year, file))
 
-        spec = librosa.feature.melspectrogram(data, sr)
+        spec = librosa.feature.melspectrogram(data.astype(np.float), sr)
 
-        with open('../features/{}/mfcc/{}.mfcc'.format(args.year, file.split('.')[0]), 'w') as out_file:
+        with open('../features/{}/mel/{}.mel'.format(args.year, file.split('.')[0]), 'w') as out_file:
             yaml.dump(spec, out_file)
 
 def dump_mfcc_features():
@@ -46,7 +47,9 @@ def dump_mfcc_features():
 
 
 def main():
-    dump_mfcc_features()
+    # dump_mfcc_features()
+    # dump_cqt_specs()
+    dump_mel_specs()
 
 if __name__ == '__main__':
     main()
