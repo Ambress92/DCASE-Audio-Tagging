2a8a7911d36f55e0454af14a6573071f2f59c03b
msBluemchen <msBluemchen@users.noreply.github.com>
2019-03-26 10:23:42 +0100

    allow to read test files

--- Audio_Tagging/dataloader.py
+++ Audio_Tagging/dataloader.py
@@ -2,6 +2,7 @@ from scipy.io import wavfile
 import tqdm
 import numpy as np
 import yaml
+import os
 
 def get_verified_files_dict(year):
     with open('../datasets/{}/train.csv'.format(year), 'r') as in_file:
@@ -20,6 +21,29 @@ def get_unverified_files_dict(year):
     unverified_files_dict = {line.split(',')[0]: line.split(',')[1] for line in data_config if line.split(',')[2].rstrip() == '1'}
 
     return unverified_files_dict
+
+def get_test_files_dict(year):
+    """
+    Determines the ground-truth label of test audio samples.
+
+    Parameters
+    ----------
+    year : int
+        Which year the data is to be taken from.
+
+    Returns
+    -------
+    test_dict : dictionary
+        Dictionary containing the name of an audio sample, and its
+        true label.
+    """
+    with open('../datasets/{}/test_post_competition.csv'.format(year), 'r') as in_file:
+        data_config = in_file.readlines()
+        data_config = data_config[1:]
+
+    test_dict = {line.split(',')[0]: line.split(',')[1] for line in data_config if line.split(',')[1].rstrip() != 'None'}
+    return test_dict
+
 
 def load_verified_files(year, features=None):
     verified_files_dict = get_verified_files_dict(year)
@@ -54,6 +78,42 @@ def load_unverified_files(year, features=None):
 
     return unverified_files
 
+def load_test_files(year, features=None):
+    """
+    Loads and returns test audio files of given year.
+
+    Parameters
+    ----------
+    year : int
+        Which year the data is to be taken from.
+    features : String
+        String containing name of feature that should be loaded.
+        If 'None', raw data is loaded.
+
+    Returns
+    -------
+    test_files : List of Tuples
+        List containing (data, label) tupels for all test audio clips.
+    """
+    test_files_dict = get_test_files_dict(year)
+
+    # load test clips
+    test_files = []
+    for file, label in tqdm.tqdm(zip(test_files_dict.keys(), test_files_dict.values()),
+                                 'Loading test clips'):
+        if not features:
+            _, data = wavfile.read('../datasets/{}/audio_test/{}'.format(year, file))
+        else:
+            if not os.path.exists('../features/test/{}/{}'.format(year, features)):
+                print('\nPlease extract features prior to loading!\n')
+                return
+            with open('../features/test/{}/{}/{}'.format(year, features, file.replace('wav', features)), 'r') as in_file:
+                data = yaml.load(in_file)
+
+        test_files.append((data, label))
+
+    return test_files
+
 def get_label_mapping(year):
     verified_files_dict = get_verified_files_dict(year)
     labels = np.unique(list(verified_files_dict.values()))
@@ -62,6 +122,24 @@ def get_label_mapping(year):
     return label_mapping, inv_label_mapping
 
 def one_hot_encode(label, num_classes):
+    """
+    Derives the one-hot-encoding representation of defined
+    label for given number of classes.
+
+    Parameters
+    ----------
+    label : int
+        Label to be one-hot-encoded.
+    num_classes : int
+        Total number of different classes.
+
+    Returns
+    -------
+    encoding : Array
+        Array containing one-hot-encoding, where label-th
+        value is set to 1., and remaining values are 0.
+    """
+
     encoding = np.zeros((num_classes))
     encoding[label] = 1
     return encoding
